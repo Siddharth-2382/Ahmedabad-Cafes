a0b37e325cddfe98c3ecc17a4000b5b97d88d596
Siddharth Movaliya <siddraj1992@gmail.com>
2022-10-05 15:41:07 +0530

    Added play again feature

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,6 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 win_series = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+
+
+def clear_board():
+    board[0] = board[1] = board[2] = board[3] = board[4] = board[5] = board[6] = board[7] = board[8] = " "
 
 
 def display_board():
@@ -14,7 +17,7 @@ def display_board():
 
 
 def ask_user_input(user_info):
-    user_input = int(input(f"Player-{user_info[0]}'s turn. Choose a number to place your tic.\n"))
+    user_input = int(input(f"Player-{user_info[0]}'s turn. Choose a number to place your tic.\t"))
     place_tic(user_input - 1, user_info[1])
 
 
@@ -29,18 +32,28 @@ def is_win():
             return True
 
 
-win = False
-user1 = [1, "X"]
-user2 = [2, "O"]
+def play_again():
+    again = input("Enter Y to play again and N to exit.\t").upper()
+    if again == "Y":
+        clear_board()
+        start_game()
+    else:
+        return False
 
-display_board()
 
-while not win:
-    ask_user_input(user1)
-    if is_win():
-        print(f"'{user1[1]}' wins!")
-        break
-    ask_user_input(user2)
-    if is_win():
-        print(f"'{user1[1]}' wins!")
-        break
+def start_game():
+    win = False
+    users = [[1, "X"], [2, "O"]]
+
+    display_board()
+
+    while not win:
+        for user in users:
+            ask_user_input(user)
+            if is_win():
+                print(f"'{user[1]}' wins!")
+                if not play_again():
+                    break
+
+
+start_game()
